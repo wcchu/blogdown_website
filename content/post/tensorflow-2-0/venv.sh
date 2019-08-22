bb9fd61978cc6dbaef85fbd77373d09279295fa5
Wei-Chun <chu@fitanalytics.com>
2019-08-22 10:55:04 +0200

    virtualenv changes

--- content/post/tensorflow-2-0/venv.sh
+++ content/post/tensorflow-2-0/venv.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
-prefix=.env
+
+# choose between tensorflow==1.14.0 and tensorflow==2.0.0-beta1 in 2 different envs
+
+prefix=.env1
 rm -r $prefix
 virtualenv --python=/usr/bin/python3 --system-site-packages $prefix
 source $prefix/bin/activate
-pip install -r python_requirements.txt
-pip install tensorflow==2.0.0-beta1
+# easy_install -U pip # pip is included in python3
+pip install --ignore-installed --no-cache-dir --upgrade \
+	pip \
+	cython \
+	testresources \
+	jupyter \
+	tensorflow==1.14.0
